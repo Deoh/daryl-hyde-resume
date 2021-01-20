f5bbab127b106b60a1c09a4b45d03dc3b800224e
Deoh <darylhyde_13@hotmail.com>
2021-01-20 19:36:58 +0000

    Added animation to landing page

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Landing page animation
+    $("#heading-name").fadeIn(3000);
+    $("#heading-title").fadeIn(3000);
+    $("#linkedin").fadeIn(3000);
+    $("#github").fadeIn(3000);
+    $("#facebook").fadeIn(3000);
+});
