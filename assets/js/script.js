07712bf98cc7967fee55a180bbea2ee0c503b4e3
Deoh <darylhyde_13@hotmail.com>
2021-02-12 23:30:38 +0000

    Added transferable to the personal info section in the index.html, added some javascript to the navbar and updated the color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,20 @@ $(document).ready(function () {
     $("#linkedin").fadeIn(3000);
     $("#github").fadeIn(3000);
     $("#facebook").fadeIn(3000);
+    $("#facebook").fadeIn(3000);
+
+    // Navbar anchor link aniamation
+    $("a[href*='#']").click(function () {
+        $(".homepage-bg").animate({height: "50vh"}, 1500);
+        return false // stops anchor refreshing the page
+    });
+
+    // Navbar
+    $(window).scroll(function() {
+    if ($(document).scrollTop() > 150) {
+      $(".cv-nav").addClass("bg-color-nav");
+    } else {
+      $(".cv-nav").removeClass("bg-color-nav");
+    }
+  });
 });
